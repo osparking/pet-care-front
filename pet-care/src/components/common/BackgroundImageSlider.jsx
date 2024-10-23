6801df515c2367f3f48168eaf0a3a914d7d49ce6
osparking <jbpark03@gmail.com>
2024-10-23 13:00:54 +0900

    create background image slider - 1st shape

---
+++ pet-care/src/components/common/BackgroundImageSlider.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import bg1 from "../../assets/images/bg1.png"
+import bg from "../../assets/images/bg.png"
+import bg3 from "../../assets/images/bg3.png"
+const BackgroundImageSlider = () => {
+    const backgrounds = [bt1, bg, bg3];
+    const [index, setIndex] = useState(0);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BackgroundImageSlider
